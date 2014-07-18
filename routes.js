6086e2d304dbe17c991f55a8e1e7775d1efbeca4
iteles <code@inesteles.com>
2014-07-18 22:03:58 +0100

    Added some very simple dummy data to use for creating the routes + tests)

--- routes.js
+++ routes.js
@@ -6,10 +6,10 @@ var Joi = require('joi'); //remove if I end up not doing any validation here
 module.exports = [
   { path: '/{friendName}',
     method: 'GET',
-    // config: {
+    config: {
       handler: viewFriendProfile,
       // view: "template-friend-profile.html"
-    // }
+    }
   }
 ];
 
@@ -19,3 +19,25 @@ function viewFriendProfile(request,reply){
   var name = request.params.friendName.split('/');
   reply("Hello "+ name[0]);
 }
+
+
+//********* DATA for now in lieu of a database ***************
+var friends = [
+  { firstName:'Anna',
+    lastName: 'Finlayson',
+    photo: "./img/person1.jpg",
+    relationship: 'friend',
+    gender: 'female',
+    birthday: '24-October-1989',
+    anniversary: '4-November-2013',
+    cards: ['./img/card1.jpg', './img/card2.jpg', './img/card3.jpg', './img/card4.jpg' ] //not sure if this is the right was to add this data!
+  },
+  { firstName:'Tyler',
+    lastName: 'Durden',
+    photo: "./img/person2.jpg",
+    relationship: 'brother',
+    gender: 'male',
+    birthday: '21-March-1978',
+    cards: ['./img/card1.jpg', './img/card3.jpg']
+  }
+];
