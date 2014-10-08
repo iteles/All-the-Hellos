35d50385686ba8130c2547432342a3a19cdcc576
iteles <code@inesteles.com>
2014-10-08 18:24:16 +0100

    Added empty placeholder controllers and started adapting view-friend-profile to understand what I need to learn

--- angular app/main.js
+++ angular app/main.js
@@ -2,8 +2,26 @@ var appModule = angular.module('allTheHellos',[]);
 
 appModule.controller("FriendListController",function($scope) {
     $scope.friends = friendsdb;
+    //pulls the info for all friends
 });
 
+appModule.controller("FriendInfoController",function($scope) {
+    $scope.friend = friendsdb[0];
+    //this controller needs to display the info for ONE friend WITHOUT being hardcoded as it is now
+});
+
+appModule.controller("CardsDisplayController", function($scope){
+    $scope.cards = friendsdb[0].cards;
+      //this controller needs to display the info for ONE friend WITHOUT being hardcoded as it is now
+});
+
+appModule.controller('ProfileMessageController', function($scope){
+    //TBD
+});
+
+appModule.controller('ImportantDatesController', function($scope){
+    //TBD
+});
 
 
 //********* DATA for now in lieu of a database ***************
@@ -15,7 +33,7 @@ var friendsdb = [
     gender: 'female',
     birthday: '24-October-1989',
     anniversary: '4-November-2013',
-    cards: ['/img/card1.jpg', '/img/card2.jpg', '/img/card3.jpg', '/img/card4.jpg' ] //not sure if this is the right was to add this data!
+    cards: [{cardURL:'img/card1.jpg'},{cardURL:'img/card2.jpg'},{cardURL:'img/card3.jpg'},{cardURL:'img/card4.jpg'}, ] //not sure if this is the right was to add this data!
   },
   { firstName:'Tyler',
     lastName: 'Durden',
@@ -23,6 +41,6 @@ var friendsdb = [
     relationship: 'brother',
     gender: 'male',
     birthday: '21-March-1978',
-    cards: ['/img/card1.jpg', '/img/card3.jpg']
+    cards: [{cardURL:'img/card1.jpg'},{cardURL:'img/card3.jpg'},]
   }
 ];
