ea67efd30cf14ea99d308fcaed659bd37803e49f
iteles <code@inesteles.com>
2014-10-15 20:57:29 +0100

    Extracted data to a factory for future connection with server

--- angular-app/friendlist.js
+++ angular-app/friendlist.js
@@ -1,40 +1,40 @@
-angular.module('allFriends', [])
-  .factory('friendlist', [function(){
-    var f = {
-      friendlist: [
-      {
-        firstName: 'Anna',
-        lastName: 'Finlayson',
-        photo: "img/person1.jpg",
-        relationship: 'friend',
-        gender: 'female',
-        dates: [ //ISO 8601 datetime string formats - lots of validation will be required here
-          {label: 'birthday', date: '1989-10-24'},
-          {label: 'anniversary', date: '2013-11-04'}],
-        cards: ["img/card1.jpg","img/card2.jpg","img/card3.jpg", "img/card4.jpg"]
-      },
-      { firstName:'Tyler',
-        lastName: 'Durden',
-        photo: "img/person2.jpg",
-        relationship: 'brother',
-        gender: 'male',
-        dates: [ //ISO 8601 datetime string formats - lots of validation will be required here
-          {label: 'birthday', date: '1978-03-21'} ],
-        cards: ["img/card1.jpg","img/card3.jpg"]
-      },
-      { firstName:'Sammy',
-        lastName: 'NoCards',
-        photo: "img/person3.jpg",
-        relationship: 'strangerdanger',
-        gender: 'female',
-        dates: [ //ISO 8601 datetime string formats - lots of validation will be required here
-          {label: 'women\'s day', date: '2014-03-08'},
-          ],
-        cards: []
-      }
-       ] //end of friendsdb
-     }; //end of var f
+var friendsModule = angular.module('FriendsModule', []);
 
-     return f;
-
-  }]);
+//Set up this service factory to create the friendlist interface with the server
+  friendsModule.factory('Friendlist', function(){
+    return [
+        { id: 1,
+          firstName: 'Anna',
+          lastName: 'Finlayson',
+          photo: "img/person1.jpg",
+          relationship: 'friend',
+          gender: 'female',
+          dates: [ //ISO 8601 datetime string formats - lots of validation will be required here
+            {label: 'birthday', date: '1989-10-24'},
+            {label: 'anniversary', date: '2013-11-04'}],
+          cards: ["img/card1.jpg","img/card2.jpg","img/card3.jpg", "img/card4.jpg"]
+        },
+        { id: 2,
+          firstName:'Tyler',
+          lastName: 'Durden',
+          photo: "img/person2.jpg",
+          relationship: 'brother',
+          gender: 'male',
+          dates: [ //ISO 8601 datetime string formats - lots of validation will be required here
+            {label: 'birthday', date: '1978-03-21'} ],
+          cards: ["img/card1.jpg","img/card3.jpg"]
+        },
+        { id: 3,
+          firstName:'Sammy',
+          lastName: 'NoCards',
+          photo: "img/person3.jpg",
+          relationship: 'strangerdanger',
+          gender: 'female',
+          dates: [ //ISO 8601 datetime string formats - lots of validation will be required here
+            {label: 'women\'s day', date: '2014-03-08'},
+            ],
+          cards: []
+        }
+       ]; //end of return
+    // }; //end of friendlist.query
+  });
--- angular-app/main.js
+++ angular-app/main.js
@@ -1,39 +1,37 @@
-var appModule = angular.module('allTheHellos',['allFriends']);
+var appModule = angular.module('allTheHellos',['FriendsModule']);
 
 appModule.controller("FriendListController",function($scope) {
-  //TEMP  $scope.friends = friendsdb;
+    $scope.friends = friendsdb;
     //pulls the info for all friends
 });
 
-appModule.controller("FriendInfoController", ['$http', 'friendlist', function($scope, friendlist) {
-    $scope.friend = friendlist[0];
-    console.log('list: '+friendlist.friendlist[0]);
-
-    // var friendlist = this;
-
-    // $http.get('/friendlist.json').success(function(data){
-    //   friendlist.friend = data;
-    // });
+appModule.controller("FriendInfoController", function($scope, Friendlist) {
+    console.log('friendlist data: ');
+    // console.log(Friendlist);
+    Friendlist.map(function(f){
+      console.log(f.lastName);
+    })
+    $scope.friend = friendsdb[0];
     //ZZZ - this controller needs to display the info for THE friend selected WITHOUT being hardcoded as it is now
-}]);
+});
 
 appModule.controller("CardsDisplayController", function($scope){
-  //TEMP  $scope.cards = friendsdb[0].cards;
+    $scope.cards = friendsdb[0].cards;
       //this controller needs to display the info for THE friend selected WITHOUT being hardcoded as it is now
 });
 
 appModule.controller('ProfileMessageController', function($scope){
   //this controller needs to display the info for THE friend selected WITHOUT being hardcoded as it is now
-//TEMP    if(friendsdb[0].cards.length){
-//       $scope.profileMessage = 'Yay! You\'re a great friend';
-//     }
-//     else{
-//       $scope.profileMessage = 'No cards yet!';
-//     }
+    if(friendsdb[0].cards.length){
+      $scope.profileMessage = 'Yay! You\'re a great friend';
+    }
+    else{
+      $scope.profileMessage = 'No cards yet!';
+    }
 });
 
 appModule.controller('ImportantDatesController', function($scope){
-//TEMP    $scope.dates = friendsdb[0].dates;
+    $scope.dates = friendsdb[0].dates;
       //this controller needs to display the info for THE friend selected WITHOUT being hardcoded as it is now
 
       //ZZZ - Custom capitalization filter to review: http://puigcerber.com/2014/05/06/angularjs-capitalize-filter/
@@ -41,35 +39,35 @@ appModule.controller('ImportantDatesController', function($scope){
 
 
 //********* DATA for now in lieu of a database ***************
-// var friendsdb = [
-// {
-//   firstName:Anna,
-//   lastName: 'Finlayson',
-//   photo: "img/person1.jpg",
-//   relationship: 'friend',
-//   gender: 'female',
-//   dates: [ //ISO 8601 datetime string formats - lots of validation will be required here
-//     {label: 'birthday', date: '1989-10-24'},
-//     {label: 'anniversary', date: '2013-11-04'}],
-//   cards: ["img/card1.jpg","img/card2.jpg","img/card3.jpg", "img/card4.jpg"]
-// },
-// { firstName:'Tyler',
-//   lastName: 'Durden',
-//   photo: "img/person2.jpg",
-//   relationship: 'brother',
-//   gender: 'male',
-//   dates: [ //ISO 8601 datetime string formats - lots of validation will be required here
-//     {label: 'birthday', date: '1978-03-21'} ],
-//   cards: ["img/card1.jpg","img/card3.jpg"]
-// },
-// { firstName:'Sammy',
-//   lastName: 'NoCards',
-//   photo: "img/person3.jpg",
-//   relationship: 'strangerdanger',
-//   gender: 'female',
-//   dates: [ //ISO 8601 datetime string formats - lots of validation will be required here
-//     {label: 'women\'s day', date: '2014-03-08'},
-//     ],
-//   cards: []
-// }
-//  ];
+var friendsdb = [
+{
+  firstName: 'Anna',
+  lastName: 'Finlayson',
+  photo: "img/person1.jpg",
+  relationship: 'friend',
+  gender: 'female',
+  dates: [ //ISO 8601 datetime string formats - lots of validation will be required here
+    {label: 'birthday', date: '1989-10-24'},
+    {label: 'anniversary', date: '2013-11-04'}],
+  cards: ["img/card1.jpg","img/card2.jpg","img/card3.jpg", "img/card4.jpg"]
+},
+{ firstName:'Tyler',
+  lastName: 'Durden',
+  photo: "img/person2.jpg",
+  relationship: 'brother',
+  gender: 'male',
+  dates: [ //ISO 8601 datetime string formats - lots of validation will be required here
+    {label: 'birthday', date: '1978-03-21'} ],
+  cards: ["img/card1.jpg","img/card3.jpg"]
+},
+{ firstName:'Sammy',
+  lastName: 'NoCards',
+  photo: "img/person3.jpg",
+  relationship: 'strangerdanger',
+  gender: 'female',
+  dates: [ //ISO 8601 datetime string formats - lots of validation will be required here
+    {label: 'women\'s day', date: '2014-03-08'},
+    ],
+  cards: []
+}
+ ];
