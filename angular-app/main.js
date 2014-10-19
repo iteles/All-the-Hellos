f256f19c01d42100437cd9dfeebd5a899ab7d694
iteles <code@inesteles.com>
2014-10-19 23:09:27 +0100

    Changed controllers to follow array-style injection format as this is good practice

--- angular-app/main.js
+++ angular-app/main.js
@@ -16,18 +16,18 @@ appModule.config([ '$routeProvider', '$locationProvider', function($routeProvide
   $locationProvider.html5Mode(true);
 }]);
 
-appModule.controller("FriendListController",function($scope, Friendlist) {
+appModule.controller("FriendListController",['$scope', 'Friendlist', function($scope, Friendlist) {
     $scope.friends = Friendlist;
     //pulls the info for all friends
-});
+}]);
 
-appModule.controller("FriendInfoController", function($scope, Friendlist, $routeParams) {
+appModule.controller("FriendInfoController",['$scope', 'Friendlist', '$routeParams', function($scope, Friendlist, $routeParams) {
     //console.log('$routeParams is: '+ $routeParams.id);
     $scope.friend = Friendlist[$routeParams.id - 1];
     //ZZZ - this array ID is precarious, what if the person's id is 64 but they're in the array at position 30 for some reason?
-});
+}]);
 
-appModule.controller('ProfileMessageController', function($scope, Friendlist){
+appModule.controller('ProfileMessageController', ['$scope', 'Friendlist', function($scope, Friendlist){
   //this controller needs to display the info for THE friend selected WITHOUT being hardcoded as it is now
     if(Friendlist[0].cards.length){
       $scope.profileMessage = 'Yay! You\'re a great friend';
@@ -35,11 +35,11 @@ appModule.controller('ProfileMessageController', function($scope, Friendlist){
     else{
       $scope.profileMessage = 'No cards yet!';
     }
-});
+}]);
 
-appModule.controller('ImportantDatesController', function($scope, Friendlist){
+appModule.controller('ImportantDatesController',['$scope', 'Friendlist', function($scope, Friendlist){
     $scope.dates = Friendlist[0].dates;
       //this controller needs to display the info for THE friend selected WITHOUT being hardcoded as it is now
 
       //ZZZ - Custom capitalization filter to review: http://puigcerber.com/2014/05/06/angularjs-capitalize-filter/
-});
+}]);
