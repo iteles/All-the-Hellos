7d4c7fec21b7e64dbd9c0aa894cc66cc991f57d5
iteles <code@inesteles.com>
2014-12-11 20:40:06 +0000

    Fixed image issue by changing extensions in img

--- meteor-app/client/templates/view_profile.js
+++ meteor-app/client/templates/view_profile.js
@@ -3,7 +3,19 @@ Template.viewProfile.helpers({
   //this._id is the ID of the current frient profile being displayed
   cards: function(){
      return Cards.find({friendId: this._id});
-  }
+  },
 
+  profileMessage: function(){
+    var message;
+    if (cards){
+      message = "Yay! You\'re a great friend!";
+    }
+    else{
+      message = "No cards yet!";
+    }
+
+    console.log(message);
+    return message;
+  }
 
 });
