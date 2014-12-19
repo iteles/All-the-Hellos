4220e1a11e745e339e6f918062c058e024167746
iteles <code@inesteles.com>
2014-12-19 23:20:06 +0000

    Fixed data context for view-card page

--- meteor-app/client/templates/people_display.js
+++ meteor-app/client/templates/people_display.js
@@ -1,7 +1,8 @@
-Template.viewCard.helpers({
-  friend: function(friendId){
-    var person= Friends.find(friendId).fetch();
-    console.log(person);
+Template.peopleDisplay.helpers({
+  //'f' here is the data context passed in in the template in {{#each friend this}}
+  //where this is the data context - in this case it's the friendId property of the card
+  friend: function(f){
+    var person= Friends.find({_id:f}).fetch();
     return person;
   }
 });
