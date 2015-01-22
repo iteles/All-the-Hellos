6bd6bea7ee642f44000b8861351cdaf2221372e7
iteles <code@inesteles.com>
2015-01-22 12:45:09 +0000

    First functioning test added

--- meteor-app/tests/mocha/client/tests.js
+++ meteor-app/tests/mocha/client/tests.js
@@ -2,18 +2,24 @@ if (typeof MochaWeb != 'undefined') {
   MochaWeb.testOnly(function () {
 
     describe('Friends are added successfully', function () {
+      var friendId = Friends.insert(
+      { firstName: 'New',
+      lastName: 'Friend'});
 
-      it('Should add a new person to the Friend collection', function(done) {
-        var friendId = Friends.insert(
-        { firstName: 'New',
-        lastName: 'Friend'});
+      it('New friend should exist in the Friends collection', function(done) {
 
         var friend = Friends.findOne({'firstName':'New'});
-        console.log(friend);
-        chai.assert.equal(friend.length === 1);
+        //friend will be an object & not and array (so no length property)
+        //using Object.keys(obj) to determine that friend actually exists
+        //and has the expected 3 keys (id, firstName and lastName)
+        chai.expect(Object.keys(friend)).to.have.length(3);
         done();
       });
 
-    });
+      // it('New test', function(done){
+      //   //ZZZ - code to be added
+      //   done();
+      // });
+    }); //end describe
   });
 }
